4ed88a0c1b3aaf9bfe2bd729b8b02f35926aa259
walwy004 <a1774783@adelaide.edu.au>
2026-09-16 18:14:48 +0930

    Create GlobalStatsController regression test

---
+++ Assignment1/src/test/java/comp3011/assignment1/GlobalStatsControllerTest.java
@@ -0,0 +1,42 @@
+package comp3011.assignment1;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import comp3011.assignment1.controller.GlobalStatsController;
+import comp3011.assignment1.dto.GlobalStatsResponse;
+import comp3011.assignment1.service.GlobalStatsService;
+
+@WebMvcTest(GlobalStatsController.class)
+class GlobalStatsControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private GlobalStatsService globalStatsService;
+
+    @Test
+    void returnsGlobalStats() throws Exception {
+
+        when(globalStatsService.getGlobalStats())
+                .thenReturn(
+                        new GlobalStatsResponse(1000, 500)
+                );
+
+        mockMvc.perform(
+                get("/api/v1/global/stats")
+        )
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.inputTokens").value(1000))
+        .andExpect(jsonPath("$.outputTokens").value(500));
+    }
+}
